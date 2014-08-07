b3ad192ec1554dca62d3f53598142cd7345fef86
aaronnw <aaronnw11@gmail.com>
2014-08-07 13:45:00 -0500

    Initial commit

---
+++ Sandbox/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Created by Aaron on 7/31/2014.
+ */
+public class Main{
+    public static void main(String[] args) {
+        String binary;
+        int digits;
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Enter a binary number");
+
+        binary = input.nextLine();
+        digits = String.valueOf(binary).length();
+        System.out.println(digits);
+
+    }
+
+}
