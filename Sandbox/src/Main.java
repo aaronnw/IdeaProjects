fc8ba1275926f964e6269127e2e21ebc971c155e
aaronnw <aaronnw11@gmail.com>
2014-08-19 12:54:11 -0500

    From intellij

--- Sandbox/src/Main.java
+++ Sandbox/src/Main.java
@@ -8,7 +8,6 @@ public class Main{
         String binary;
         int digits;
         Scanner input = new Scanner(System.in);
-        String[] places;
 
 
         System.out.println("Enter a binary number");
@@ -16,7 +15,6 @@ public class Main{
         binary = input.nextLine();
         digits = String.valueOf(binary).length();
         System.out.println(digits);
-
     }
 
 }
